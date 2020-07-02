cc111c7db26e3863f80908d5abe472db50519e77
Your Namekinderzyc <75829215@qq.com>
2020-07-02 13:41:07 +0800

    deploy

---
+++ precache-manifest.f94e82142cd578f4b95531bb531c55e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59120c9ba02bea5c70770330fa1943ac",
+    "url": "/BookKeeping-website/index.html"
+  },
+  {
+    "revision": "06d81253d3dcd0148b28",
+    "url": "/BookKeeping-website/static/css/main.10f8e779.chunk.css"
+  },
+  {
+    "revision": "25183da67120eaf2c835",
+    "url": "/BookKeeping-website/static/js/2.aa2a7e8a.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/BookKeeping-website/static/js/2.aa2a7e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d81253d3dcd0148b28",
+    "url": "/BookKeeping-website/static/js/main.9594b2fa.chunk.js"
+  },
+  {
+    "revision": "6cbd29ae5f8621e92d7f",
+    "url": "/BookKeeping-website/static/js/runtime-main.833b8a94.js"
+  }
+]);
